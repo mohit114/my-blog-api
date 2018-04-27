33ba8b467992ec8f547016f659eee21b7971ff3f
Mohit Maharjan <mohitmaharjan7@gmail.com>
2018-04-27 18:31:55 +0545

    removed bcrypt

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const userModel = require('../models/user');
 const contactModel = require('../models/contact');
 
@@ -9,40 +8,6 @@ router.get('/', (req, res, next) => {
        "Title": "Welcome to blog api"
     });
 });
-
-router.get('/user', (req, res, next) => {
-   userModel.findOne({'username' : 'mohitmaharjan7@gmail.com'}, function(err, user){
-        if(user)
-            res.status(200).json(user);
-        });
-});
-
-
-router.post('/add', (req, res, next) => {
-    userModel.findOne({'username':req.body.username}, function(err, user){
-        //check if user exists. if exists return the user
-        if(user){
-            res.status(200).json({'existing_user': user});
-        }
-        // if user does not exists then create a new user and return that newly created user
-        else{
-            var newUser = new userModel({
-                name: req.body.name,
-                username: req.body.username,
-                password: bcrypt.hashSync(req.body.password, 10),
-                isAdmin : req.body.isAdmin,
-                createdDate: Date.now(),
-                isLocked: req.body.isLocked
-            });
-            newUser.save().then(result => {
-                res.status(200).json({'newlyAddedUser': result});
-            }).catch(err => {
-                res.status(400).send("Unable to save to database. Error : " + err.message);
-            });
-        }
-    });
-});
-
 router.post('/contact', (req, res, next) => {
     var newContact = new contactModel({
         fullName: req.body.fullName,
